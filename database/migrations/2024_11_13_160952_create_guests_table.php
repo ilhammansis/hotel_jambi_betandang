828fee351bffd6fa84a480b2af1884cec3589953
Code-5 <109147548+ilhammansis@users.noreply.github.com>
2024-11-13 23:17:31 +0700

    commit model & migration

---
+++ database/migrations/2024_11_13_160952_create_guests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->id();
+            $table->string('guest_name', 50);
+            $table->string('email', 50)->unique();
+            $table->string('phone_number', 15);
+            $table->text('address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guests');
+    }
+};
